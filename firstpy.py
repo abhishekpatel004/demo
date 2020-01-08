69cb7619c616e5061717d8e815ab9bcffe631026
poojamalhotra <jenil.jain@somaiya.edu>
2020-01-08 14:27:37 +0530

    files added

---
+++ firstpy.py
@@ -0,0 +1,33 @@
+#print "hello"
+
+print "---comments---"
+print 'single line'   "#"
+
+print 'multiline (""")'
+
+print "---datatypes---"
+a=18                    #int data type
+
+a1='hms'                #string data type
+
+b=135.45                #float data type
+
+              #expression
+
+c=12                    #complex dat type
+
+d=59
+e=c+d
+print "sum is ",e
+
+
+
+
+f=float(a)
+
+print "int to float",f
+
+h=oct(d)
+
+print"decimal to oct",h
+
